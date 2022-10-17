75c969837d4736224cb6116188c00081b023765b
matiasperlo <103325934+matiasperlo@users.noreply.github.com>
2022-10-17 12:16:43 -0300

    jwt funcionando con detalles
    La autenticacion y control de acceso funcional pero con detalles:
    - Si haces un login con credenciales distintas a las hardcodeadas no te devuelve ningun forbidden.
    - Si haces get a cualquier recurso sin el Bearer tampoco te da forbidden, solo devuelve un response vacío.
    
    Posiblemente sea una exception manejada por el propio spring.

---
+++ src/main/java/io/github/matiasperlo/portfolio/dto/AuthenticationRequest.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.github.matiasperlo.portfolio.dto;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author matia
+ */
+public class AuthenticationRequest implements Serializable {
+
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //need default constructor for JSON Parsing
+    public AuthenticationRequest()
+    {
+
+    }
+
+    public AuthenticationRequest(String username, String password) {
+        this.setUsername(username);
+        this.setPassword(password);
+    }
+}
+
---
+++ src/main/java/io/github/matiasperlo/portfolio/service/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.github.matiasperlo.portfolio.service;
+
+import java.util.ArrayList;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author matia
+ */
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return new User("foo", "foo",
+                new ArrayList<>());
+    }
+}
